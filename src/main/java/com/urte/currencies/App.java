a9b00c3ce2a1871be7e854fd0a152ba7bda82254
Urte <urte.rimsaite@gmail.com>
2019-11-13 12:49:05 +0200

    Add currencies input validation

--- src/main/java/com/urte/currencies/App.java
+++ src/main/java/com/urte/currencies/App.java
@@ -10,11 +10,15 @@ import java.util.stream.Collectors;
 public class App
 {
     final static String DELIMITER = " ";
-    private static Pattern DATE_PATTERN = Pattern.compile("^\\d{4}-\\d{2}-\\d{2}$");
-    private static String DATE_ERROR = "Enter at least two dates in format YYYY-MM-DD";
 
-    public static void main( String[] args )
-    {
+    private static Pattern DATE_PATTERN = Pattern.compile("^\\d{4}-\\d{2}-\\d{2}$");
+    private static Pattern CURRENCY_PATTERN = Pattern.compile("[a-zA-Z]{3}");
+
+    private static String DATE_ERROR = "Enter at least two dates in format YYYY-MM-DD";
+    private static String CURRENCY_ERROR = "Enter at least one valid currency code";
+
+    public static void main( String[] args ) {
+
         Scanner in = new Scanner(System.in);
 
         System.out.println("Please enter dates");
@@ -22,21 +26,34 @@ public class App
         String datesInput = in.nextLine();
         List<LocalDate> dates = Arrays
                 .stream(datesInput.split(DELIMITER))
+                .map(date -> date.trim())
                 .filter(date -> DATE_PATTERN.matcher(date).matches())
                 .map(date -> LocalDate.parse(date))
                 .limit(2)
                 .sorted()
                 .collect(Collectors.toList());
-        dates.forEach(date -> System.out.println("You entered date: "+date.toString()));
+
+        dates.forEach(date -> System.out.println("You entered date: " + date.toString()));
 
         if (dates.size() < 2) {
             exit(DATE_ERROR);
         }
 
         System.out.println("Please enter currency codes");
+
         String currenciesInput = in.nextLine();
-        System.out.println("You entered currencies: "+currenciesInput);
-        String[] currencies = currenciesInput.split(DELIMITER);
+        List<String> currencies = Arrays
+                .stream(currenciesInput.split(DELIMITER))
+                .map(currency -> currency.trim())
+                .filter(currency -> CURRENCY_PATTERN.matcher(currency).matches())
+                .map(currency -> currency.toUpperCase())
+                .collect(Collectors.toList());
+
+        currencies.forEach(currency -> System.out.println("You entered currency: " + currency));
+
+        if(currencies.isEmpty()) {
+            exit(CURRENCY_ERROR);
+        }
     }
 
     static void exit(String message) {
